afb8621d6d3a838177ebecc18250fe70246e3a91
franchescofamous <franchescofamous@gmail.com>
2022-08-25 09:16:57 +0000

    starting if/else conditions

---
+++ partie1/index.js
@@ -0,0 +1,6 @@
+let fstNum = Number(prompt("entrer le 1er numero:"));
+let sndNum = Number(prompt("entrer le 2eme numero:"));
+
+let result = fstNum * sndNum;
+
+isNaN(result) ? alert("restart") : alert("bravoooooo");
