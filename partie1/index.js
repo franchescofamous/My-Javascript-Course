09b5a5a7f28eb44d315bb2d987b06b5830726aa6
franchescofamous <franchescofamous@gmail.com>
2022-08-25 10:47:32 +0000

    exo2 resolu

--- partie1/index.js
+++ partie1/index.js
@@ -1,6 +1,12 @@
 let fstNum = Number(prompt("entrer le 1er numero:"));
 let sndNum = Number(prompt("entrer le 2eme numero:"));
+isNaN(fstNum);
 
-let result = fstNum * sndNum;
-
-isNaN(result) ? alert("restart") : alert("bravoooooo");
+if (!isNaN(fstNum) && !isNaN(sndNum)) {
+  let result = fstNum + sndNum;
+  result % 2 === 0
+    ? alert("vous avez un nombre paire")
+    : alert("vous avez un nombre impaire");
+} else {
+  alert("recharger la page et choisissez un nombre");
+}
